8268e3d0f0e4578453869962287e82fc8c692591
Rishi Srivastava <rishisrivasta@gmail.com>
2023-02-18 08:48:47 +0800

    added print utils

--- src/main/java/com/backstreetbrogrammer/PrintUtils.java
+++ src/main/java/com/backstreetbrogrammer/PrintUtils.java
@@ -1,5 +1,7 @@
 package com.backstreetbrogrammer;
 
+import java.util.List;
+
 public class PrintUtils {
 
     private PrintUtils() {
@@ -8,6 +10,14 @@ public class PrintUtils {
     public static void printIntArray(final int[] array) {
         if (array == null || array.length == 0) return;
         for (final int ele : array) {
+            System.out.printf("%d, ", ele);
+        }
+        System.out.println();
+    }
+
+    public static void printIntList(final List<Integer> list) {
+        if (list == null || list.size() == 0) return;
+        for (final int ele : list) {
             System.out.printf("%d, ", ele);
         }
         System.out.println();
