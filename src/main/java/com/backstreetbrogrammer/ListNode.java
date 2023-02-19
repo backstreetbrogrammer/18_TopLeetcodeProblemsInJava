d0b09cf4889b43c093fc0b547a097ededf874205
Rishi Srivastava <rishisrivasta@gmail.com>
2023-02-20 06:26:26 +0800

    solved problem 2

---
+++ src/main/java/com/backstreetbrogrammer/ListNode.java
@@ -0,0 +1,15 @@
+package com.backstreetbrogrammer;
+
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(final int val) {
+        this.val = val;
+    }
+
+}
---
+++ src/main/java/com/backstreetbrogrammer/Q2_AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,38 @@
+package com.backstreetbrogrammer.Q2_AddTwoNumbers;
+
+import com.backstreetbrogrammer.ListNode;
+
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        final ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        int carry = 0;
+        while ((l1 != null) || (l2 != null) || (carry != 0)) {
+            int val1 = 0, val2 = 0;
+            if (l1 != null) {
+                val1 = l1.val;
+            }
+            if (l2 != null) {
+                val2 = l2.val;
+            }
+
+            int val = val1 + val2 + carry;
+            carry = val / 10;
+            val %= 10;
+            current.next = new ListNode(val);
+
+            current = current.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+}
