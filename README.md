b1cee173c8761f527b2f9f3e3b241709e0f40625
Rishi Srivastava <rishisrivasta@gmail.com>
2023-03-03 06:32:04 +0800

    solved problem 8

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 5. Maximum Subarray
 6. Maximum Product Subarray
 7. Find Minimum in Rotated Sorted Array
+8. Search in Rotated Sorted Array
 
 ### Part 2 - OTHER TOP PROBLEMS
 
@@ -156,13 +157,13 @@ Explanation: [2,3] has the largest product 6.
 
 [Find Minimum in Rotated Sorted Array](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/)
 
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
+Suppose an array of length `n` sorted in ascending order is rotated between `1` and `n` times.
 
-For example, the array nums = [0,1,2,4,5,6,7] might become:
+For example, the array `nums = [0,1,2,4,5,6,7]` might become:
 
-[4,5,6,7,0,1,2] if it was rotated 4 times.
+`[4,5,6,7,0,1,2]` if it was rotated 4 times.
 
-[0,1,2,4,5,6,7] if it was rotated 7 times.
+`[0,1,2,4,5,6,7]` if it was rotated 7 times.
 
 Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2],
 ..., a[n-2]].
--- src/main/java/com/backstreetbrogrammer/part1_top75/Q8_SearchInRotatedSortedArray/SearchInRotatedSortedArray.java
+++ src/main/java/com/backstreetbrogrammer/part1_top75/Q8_SearchInRotatedSortedArray/SearchInRotatedSortedArray.java
@@ -2,9 +2,33 @@ package com.backstreetbrogrammer.part1_top75.Q8_SearchInRotatedSortedArray;
 
 public class SearchInRotatedSortedArray {
 
-    public int search(int[] nums, int target) {
-        //TODO
-        return 0;
+    public static void main(final String[] args) {
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+    }
+
+    private static int search(final int[] nums, final int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            final int mid = l + (r - l) / 2;
+            if (target == nums[mid]) return mid;
+
+            if (nums[l] <= nums[mid]) { // left portion
+                if (target > nums[mid] || target < nums[l]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            } else { // right portion
+                if (target < nums[mid] || target > nums[r]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+        return -1; // not found
     }
 
 }
